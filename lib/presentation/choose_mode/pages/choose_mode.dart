e611ac783b34c3f246e06157100399636378d242
naumanbutt2002 <nauman100percent@gmail.com>
2024-10-11 23:08:45 +0500

    Logic Of Light-Dark Mode

--- lib/presentation/choose_mode/pages/choose_mode.dart
+++ lib/presentation/choose_mode/pages/choose_mode.dart
@@ -1,7 +1,9 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotify/presentation/choose_mode/bloc/theme_cubit.dart';
 
 import '../../../common/widgets/button/basic_app_button.dart';
 import '../../../core/configs/assets/app_images.dart';
@@ -18,53 +20,66 @@ class ChooseModePage extends StatelessWidget {
       body: Stack(
         children: [
           Container(
-            padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 40),
+            padding: const EdgeInsets.symmetric(
+              vertical: 40,
+              horizontal: 40
+            ),
             decoration: const BoxDecoration(
-                image: DecorationImage(
-                    fit: BoxFit.fill,
-                    image: AssetImage(
-                      AppImages.chooseModeBG,
-                    ))),
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage(
+                  AppImages.chooseModeBG,
+                )
+              )
+            ),
           ),
+
           Container(
             color: Colors.black.withOpacity(0.15),
           ),
+
           Padding(
-            padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 40),
+            padding: const EdgeInsets.symmetric(
+              vertical: 40,
+              horizontal: 40
+            ),
             child: Column(
-              children: [
-                Align(
-                  alignment: Alignment.topCenter,
-                  child: SvgPicture.asset(AppVectors.logo),
-                ),
-                const Spacer(),
-                const Text(
-                  'Choose Mode',
-                  style: TextStyle(
+                children: [
+                  Align(
+                    alignment: Alignment.topCenter,
+                    child: SvgPicture.asset(
+                      AppVectors.logo
+                    ),
+                  ),
+                  const Spacer(),
+                  const Text(
+                    'Choose Mode',
+                    style: TextStyle(
                       fontWeight: FontWeight.bold,
                       color: Colors.white,
-                      fontSize: 18),
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                Row(
+                      fontSize: 18
+                    ),
+                  ),
+                  const SizedBox(height: 40,),
+                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Column(
                       children: [
                         GestureDetector(
-                          onTap: () {},
+                          onTap: (){
+                            context.read<ThemeCubit>().updateTheme(ThemeMode.dark);
+                          },
                           child: ClipOval(
                             child: BackdropFilter(
-                              filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                              filter: ImageFilter.blur(sigmaX: 10,sigmaY: 10),
                               child: Container(
                                 height: 80,
                                 width: 80,
                                 decoration: BoxDecoration(
-                                    color: const Color(0xff30393C)
-                                        .withOpacity(0.5),
-                                    shape: BoxShape.circle),
+                                  color: const Color(0xff30393C).withOpacity(0.5),
+                                  shape: BoxShape.circle
+                                ),
                                 child: SvgPicture.asset(
                                   AppVectors.moon,
                                   fit: BoxFit.none,
@@ -73,36 +88,35 @@ class ChooseModePage extends StatelessWidget {
                             ),
                           ),
                         ),
-                        const SizedBox(
-                          height: 15,
-                        ),
+                        const SizedBox(height: 15,),
                         const Text(
                           'Dark Mode',
                           style: TextStyle(
-                              fontWeight: FontWeight.w500,
-                              fontSize: 17,
-                              color: AppColors.grey),
+                            fontWeight: FontWeight.w500,
+                            fontSize: 17,
+                            color: AppColors.grey
+                          ),
                         )
                       ],
                     ),
-                    const SizedBox(
-                      width: 40,
-                    ),
+                    const SizedBox(width: 40,),
                     Column(
                       children: [
                         GestureDetector(
-                          onTap: () {},
+                          onTap: (){
+                            context.read<ThemeCubit>().updateTheme(ThemeMode.light);
+                          },
                           child: ClipOval(
                             child: BackdropFilter(
-                              filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                              filter: ImageFilter.blur(sigmaX: 10,sigmaY: 10),
                               child: Container(
                                 height: 80,
                                 width: 80,
                                 decoration: BoxDecoration(
-                                    color: const Color(0xff30393C)
-                                        .withOpacity(0.5),
-                                    shape: BoxShape.circle),
-                                child: SvgPicture.asset(
+                                  color: const Color(0xff30393C).withOpacity(0.5),
+                                  shape: BoxShape.circle
+                                ),
+                                 child: SvgPicture.asset(
                                   AppVectors.sun,
                                   fit: BoxFit.none,
                                 ),
@@ -110,26 +124,33 @@ class ChooseModePage extends StatelessWidget {
                             ),
                           ),
                         ),
-                        const SizedBox(
-                          height: 15,
-                        ),
+                      const SizedBox(height: 15,),
                         const Text(
                           'Light Mode',
                           style: TextStyle(
-                              fontWeight: FontWeight.w500,
-                              fontSize: 17,
-                              color: AppColors.grey),
+                            fontWeight: FontWeight.w500,
+                            fontSize: 17,
+                            color: AppColors.grey
+                          ),
                         )
                       ],
                     ),
                   ],
-                ),
-                const SizedBox(
-                  height: 50,
-                ),
-                BasicAppButton(onPressed: () {}, title: 'Continue')
-              ],
-            ),
+                 ),
+                  const SizedBox(height: 50,),
+                  // BasicAppButton(
+                  //   onPressed: () {
+                  //     Navigator.push(
+                  //       context,
+                  //       MaterialPageRoute(
+                  //         builder: (BuildContext context) => const SignupOrSigninPage()
+                  //       )
+                  //    );
+                  //   },
+                  //   title: 'Continue'
+                  // )
+                ],
+              ),
           ),
         ],
       ),
